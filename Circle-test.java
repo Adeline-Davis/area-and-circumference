4fd93d51a35ce8ac5b7f75203cce6cbfdb557a0f
Addie <32769685+Adeline-Davis@users.noreply.github.com>
2017-10-24 19:52:15 -0400

    Add files via upload

---
+++ Circle-test.java
@@ -0,0 +1,24 @@
+package edu.cscc;
+import java.util.Scanner;
+
+// Adeline Davis - Calculate a circle's area and circumference
+
+public class Main {
+
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double radius;
+        double area;
+        double perimeter;
+
+        Scanner userInput = new Scanner (System.in);
+        String name = "";
+            System.out.print("Please enter a radius.");
+            radius = input.nextDouble();
+            area = Math.PI * radius * radius;
+            System.out.println("The area of a circle with a radius of " + radius + " is " + area);
+            perimeter = 2 * Math.PI * radius;
+            System.out.println("The circumference of a circle with a radius of " + radius + " is " + perimeter);
+        }
+    }
